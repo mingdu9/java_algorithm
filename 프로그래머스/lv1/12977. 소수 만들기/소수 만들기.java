ef8a69bbe41e78b42f2d48e3dac62c05950808bc
구민주 <74780404+mingdu9@users.noreply.github.com>
2022-11-21 20:22:03 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -10,7 +10,6 @@ class Solution {
     
     public int solution(int[] nums) {
         int answer = 0;
-        boolean check = false;
         for(int i =0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
                 for(int k=j+1;k<nums.length;k++){
