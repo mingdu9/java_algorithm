8f7525bbc7ade852986aa3dd1ad3b6263642bda6
구민주 <74780404+mingdu9@users.noreply.github.com>
2022-11-21 21:31:22 +0900

    [level 1] Title: 실패율, Time: 0.02 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.java
+++ 프로그래머스/lv1/42889. 실패율/실패율.java
@@ -1,34 +1,37 @@
 class Solution {
     public int[] solution(int N, int[] stages) {
         int[] answer = new int[N];
-    double[] rate = new double[N];
-    double sum = 0;
-    double fail = 0;
-    double max;
-    int idx = 1;
-    for (int i = 1; i <= N; i++) {
-      sum = 0;
-      fail = 0;
-      for (int stage : stages) {
-        if (i <= stage)
-          sum++;
-        if (i == stage)
-          fail++;
-      }
-      if(sum > 0) rate[i - 1] = fail / sum;
-    }
-    for (int i = 0; i < N; i++) {
-      max = rate[0];
-      idx = 1;
-      for (int j = 0; j < rate.length; j++) {
-        if (rate[j] > max) {
-          max = rate[j];
-          idx = j + 1;
+        double[] rate = new double[N];
+        double sum;
+        double fail;
+        double max;
+        int idx;
+        
+        for (int i = 1; i <= N; i++) {
+            sum = 0;
+            fail = 0;
+            for (int stage : stages) {
+                if (i <= stage)
+                    sum++;
+                if (i == stage)
+                    fail++;
+            }
+            if(sum > 0) rate[i - 1] = fail / sum;
         }
-      }
-      rate[idx - 1] = -1;
-      answer[i] = idx;
-    }
-    return answer;
+        
+        for (int i = 0; i < N; i++) {
+            max = rate[0];
+            idx = 1;
+            for (int j = 0; j < N; j++) {
+                if (rate[j] > max) {
+                    max = rate[j];
+                    idx = j + 1;
+                }
+            }
+            rate[idx - 1] = -1;
+            answer[i] = idx;
+        }
+        
+        return answer;
     }
 }
