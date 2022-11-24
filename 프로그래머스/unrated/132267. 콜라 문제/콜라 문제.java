810daf7d7236eb5b3cb548573e143d653d214b2a
구민주 <74780404+mingdu9@users.noreply.github.com>
2022-11-24 17:15:35 +0900

    [unrated] Title: 콜라 문제, Time: 0.04 ms, Memory: 82.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int result = 0;
+    while (n >= a) {
+      result += n / a * b;
+      n = n / a * b + n % a;
+    }
+
+    return result;
+    }
+}
