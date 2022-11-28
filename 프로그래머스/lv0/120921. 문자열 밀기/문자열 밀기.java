2595c99ff0accd04c1edf59217aa4ce3686b7b79
구민주 <74780404+mingdu9@users.noreply.github.com>
2022-11-28 21:28:08 +0900

    [level 0] Title: 문자열 밀기, Time: 1.80 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,13 +1,5 @@
 class Solution {
     public int solution(String A, String B) {
-        int answer = 0;
-        String tmp = A;
-        for (int i = 0; i < A.length(); i++) {
-            if (tmp.equals(B)) return answer;
-            String a = tmp.substring(A.length() - 1);
-            tmp = a + tmp.substring(0, tmp.length() - 1);
-            answer++;
-        }
-        return -1;
+        return (B+B).indexOf(A);
     }
 }
