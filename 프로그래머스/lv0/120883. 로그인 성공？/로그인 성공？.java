7a1a8a51e8dece82ed5f999c12ec05cfd4c91466
구민주 <74780404+mingdu9@users.noreply.github.com>
2022-12-01 15:57:39 +0900

    [level 0] Title: 로그인 성공?, Time: 0.02 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        for(int i=0;i<db.length;i++){
+            if(db[i][0].equals(id_pw[0]) && db[i][1].equals(id_pw[1]))  return "login";
+            if(db[i][0].equals(id_pw[0]) && !db[i][1].equals(id_pw[1])) return "wrong pw";
+        }
+        return "fail";        
+    }
+}
