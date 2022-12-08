1ea844c634e60e603765c897a2aa6f1463d1758e
구민주 <74780404+mingdu9@users.noreply.github.com>
2022-12-08 17:20:14 +0900

    [level 1] Title: 신규 아이디 추천, Time: 3.97 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String new_id) {
+    String answer = "";
+    answer = new_id.toLowerCase();
+    answer = answer.replaceAll("[^-_.\\w]|", "");
+    answer = answer.replaceAll("\\.+", ".");
+    answer = answer.replaceAll("^\\.|\\.$", "");
+    if (answer.length() >= 16){
+      answer = answer.substring(0, 15);
+      answer = answer.replaceAll("^\\.|\\.$", "");
+    }
+    if (answer.length() == 0)
+      answer = "a";
+    if (answer.length() <= 2) {
+      while (answer.length() < 3) {
+        String tmp = answer.substring(answer.length() - 1, answer.length());
+        answer = answer + tmp;
+      }
+    }
+    return answer;
+    }
+}
