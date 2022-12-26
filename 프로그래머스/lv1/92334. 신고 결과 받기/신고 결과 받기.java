1224ac4565a9609bca0cd95a35d708b39117c4e1
구민주 <74780404+mingdu9@users.noreply.github.com>
2022-12-26 15:45:38 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.26 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -2,17 +2,21 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
+        // 신고된사람 / 신고한사람들
 		Map<String, HashSet<String>> reported = new HashMap<>();
 		List<String> users = Arrays.asList(id_list);
+        // map에 신고한사람 이름 넣기
 		for (String reporter : id_list) {
 			HashSet<String> reportedUsers = new HashSet<>();
 			reported.put(reporter, reportedUsers);
 		}
+        // 신고한 사람 추가
 		for (String info : report) {
 			String reporter = info.split(" ")[0];
 			String reportedUser = info.split(" ")[1];
 			reported.get(reportedUser).add(reporter);
 		}
+        // 메일 수 더하기
 		for (String reportedUser : users) {
 			if (reported.get(reportedUser).size() >= k) {
 				for (String reporter : reported.get(reportedUser)) {
