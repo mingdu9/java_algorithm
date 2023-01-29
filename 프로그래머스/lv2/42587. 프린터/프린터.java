a8f0c2a108666540b62279eb3a509c9d624cb426
구민주 <74780404+mingdu9@users.noreply.github.com>
2023-01-30 04:52:22 +0900

    [level 2] Title: 프린터, Time: 0.55 ms, Memory: 69 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프린터/프린터.java
+++ 프로그래머스/lv2/42587. 프린터/프린터.java
@@ -7,13 +7,13 @@ class Solution {
             queue.add(priority);
         
         while(!queue.isEmpty()){
+            // 모든 문서가 프린트될 떄까지
             for(int i=0;i<priorities.length;i++) {
                 if(priorities[i] == queue.peek()){
-                    if(i == location){
-                        answer++;
-                        return answer;
-                    }
+                    // 원래 순서대로 poll, 카운터 증가
                     answer++;
+                    if(i == location)
+                        return answer;
                     queue.poll();
                 }
             }
