32f22e99542497706562e2027bf68755dd4fafc7
구민주 <74780404+mingdu9@users.noreply.github.com>
2023-01-30 09:42:30 +0900

    [level 2] Title: 짝지어 제거하기, Time: 48.02 ms, Memory: 58.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution
+{
+    public int solution(String s)
+    {
+        Stack<Character> stack = new Stack<>();
+    for(int i=0;i<s.length();i++){
+      if(stack.isEmpty()) {
+        stack.push(s.charAt(i));
+        continue;
+      }
+      if(stack.peek() == s.charAt(i)) {
+        stack.pop();
+        continue;
+      }
+      stack.push(s.charAt(i));
+    }
+
+    if(stack.isEmpty()) return 1;
+    return 0;
+    }
+}
